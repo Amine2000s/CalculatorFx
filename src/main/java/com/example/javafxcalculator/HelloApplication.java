52cee5d952ecd445f8e1549721dd11a951fdb991
Amine2000s <92222814+Amine2000s@users.noreply.github.com>
2023-10-21 15:17:01 +0100

    ADDED files with new approach

--- src/main/java/com/example/javafxcalculator/HelloApplication.java
+++ src/main/java/com/example/javafxcalculator/HelloApplication.java
@@ -1,19 +1,36 @@
 package com.example.javafxcalculator;
 
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scientifique_Calculator.fxml"));
+
+        String css1 = this.getClass().getResource("dark-theme.css").toExternalForm();
+        String css2 = this.getClass().getResource("light-theme.css").toExternalForm();
+        Parent p = fxmlLoader.load();
+        Scene scene = new Scene(p);
+        scene.getStylesheets().add(css2);
+
+        NormalCalculatorController  s1 = new NormalCalculatorController();
+
+        s1.setScene(scene);
+        s1.setTypeofCalculator();
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
